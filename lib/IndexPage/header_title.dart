ac29a42b371301729cc901bb83c681204b2c2038
DeepP2667 <patel.deep@ufl.edu>
2021-10-08 22:10:32 -0400

    organized header widgets into file

---
+++ lib/IndexPage/header_title.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class HeaderTitle extends StatelessWidget {
+  HeaderTitle({Key? key}) : super(key: key);
+
+  final TextStyle titleTextStyle = GoogleFonts.stoke(
+    textStyle: TextStyle(
+      fontSize: 25,
+      color: Colors.blue[200],
+      fontWeight: FontWeight.bold,
+    ),
+  );
+
+  final TextStyle seenTextStyle = GoogleFonts.stoke(
+    textStyle: TextStyle(
+      fontSize: 15,
+      color: Colors.blue[200]?.withOpacity(0.6),
+    ),
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Stack(
+        children: [
+          Align(
+            alignment: const Alignment(0,-0.6),
+            child: Text(
+              "INDEX",
+              style: titleTextStyle,
+            ),
+          ),
+          Align(
+            alignment: const Alignment(0,.3),
+            child: Text(
+              "Found: ",
+              style: seenTextStyle,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
