92736a96407bdc0503d442957db24090322396e6
DeepP2667 <patel.deep@ufl.edu>
2021-10-08 22:09:36 -0400

    Header of page

--- lib/IndexPage/header.dart
+++ lib/IndexPage/header.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import './header_title.dart';
 
 class IndexHeader extends StatelessWidget {
   IndexHeader({Key? key}) : super(key: key);
 
-  final TextStyle textStyle = TextStyle(
-    fontSize: 20,
-    color: Colors.grey[700],
-    fontWeight: FontWeight.bold,
+  final TextStyle textStyle = GoogleFonts.robotoMono(
+    textStyle: TextStyle(
+      fontSize: 20,
+      color: Colors.grey[700],
+      fontWeight: FontWeight.bold,
+    ),
   );
 
   final ButtonStyle buttonStyle = ElevatedButton.styleFrom(
@@ -19,18 +24,21 @@ class IndexHeader extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      height: 150.0,
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors: <Color> [
+            Color(0XFFECEFF1),
+            Color(0XFFE3F2FD),
+          ],
+        ),
+      ),
       padding: const EdgeInsets.only(top: 30.0),
-      margin: const EdgeInsets.only(top: 10.0, bottom: 10.0),
-      child: Row(
+      child: Column(
         children: [
-          ElevatedButton(
-            style: buttonStyle,
-            onPressed: null,
-            child: Text(
-              "<-",
-              style: textStyle,
-            ),
-          ),
+          HeaderTitle(),
         ],
       ),
     );
