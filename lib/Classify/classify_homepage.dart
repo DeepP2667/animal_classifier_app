87259e8d5f489d47155354da4c637b36135189f2
DeepP2667 <deeppatel2667@gmail.com>
2021-10-17 20:52:37 -0400

    added based google firebase ML for flutter

--- lib/Classify/classify_homepage.dart
+++ lib/Classify/classify_homepage.dart
@@ -1,4 +1,6 @@
 import 'dart:io';
+import 'package:google_ml_kit/google_ml_kit.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:flutter/material.dart';
 
@@ -14,8 +16,10 @@ class CameraPage extends StatefulWidget {
 
 class _CameraPageState extends State<CameraPage> {
   File? _imageFile;
-
+  late InputImage inputImage;
   late int selectedIndex;
+
+  final imageLabeler = GoogleMlKit.vision.imageLabeler();
 
   Future gallary() async {
     final imageFile =
@@ -26,10 +30,22 @@ class _CameraPageState extends State<CameraPage> {
     }
 
     final tempImageFile = File(imageFile.path);
+    inputImage = InputImage.fromFilePath(imageFile.path);
 
     setState(() {
       _imageFile = tempImageFile;
     });
+
+    final List<ImageLabel> labels = await imageLabeler.processImage(inputImage);
+
+    for (ImageLabel label in labels) {
+      final String text = label.label;
+      final int index = label.index;
+      final double confidence = label.confidence;
+      print("$text,  $index,  $confidence");
+    }
+
+    imageLabeler.close();
   }
 
   Future camera() async {
@@ -40,11 +56,40 @@ class _CameraPageState extends State<CameraPage> {
     }
 
     final tempImageFile = File(imageFile.path);
+    inputImage = InputImage.fromFilePath(imageFile.path);
 
     setState(() {
       _imageFile = tempImageFile;
     });
+
+    final List<ImageLabel> labels = await imageLabeler.processImage(inputImage);
+
+    for (ImageLabel label in labels) {
+      final String text = label.label;
+      final int index = label.index;
+      final double confidence = label.confidence;
+      print("$text,  $index,  $confidence");
+    }
+
+    imageLabeler.close();
   }
+
+  static Color buttonBackgroundColor = Color.alphaBlend(
+    const Color(0xFFcc9764),
+    const Color(0xFF573625),
+  );
+
+  final TextStyle textStyle = GoogleFonts.stoke(
+    textStyle: const TextStyle(
+      fontSize: 22,
+      color: Color(0xFF573625),
+      fontWeight: FontWeight.bold,
+    ),
+  );
+
+  final ButtonStyle buttonStyle = ElevatedButton.styleFrom(
+    primary: buttonBackgroundColor,
+  );
 
   @override
   Widget build(BuildContext context) {
@@ -67,14 +112,28 @@ class _CameraPageState extends State<CameraPage> {
             child: Stack(
               children: <Widget>[
                 ImageDisplay(imageFile: _imageFile),
+                Align(
+                  alignment: const Alignment(0, 1),
+                  child: Expanded(
+                    child: SizedBox(
+                      width: MediaQuery.of(context).size.width,
+                      height: 100,
+                      child: ElevatedButton(
+                        style: buttonStyle,
+                        onPressed: (){print("test- remove later");},
+                        child: Text("Classify", style: textStyle),
+                      ),
+                    ),
+                  ),
+                ),
               ],
             ),
           ),
         ),
         bottomNavigationBar: BottomNavigationBar(
-            backgroundColor: Color(0xFFf7e8e3),
-            unselectedItemColor: Color(0xFF573625),
-            selectedItemColor: Color(0xFF573625),
+            backgroundColor: const Color(0xFFf7e8e3),
+            unselectedItemColor: const Color(0xFF573625),
+            selectedItemColor: const Color(0xFF573625),
             items: const [
               BottomNavigationBarItem(
                 icon: Icon(Icons.arrow_back_ios_new_outlined),
